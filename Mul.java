95347811931dc74dc829f9c56d89dd826c3744e5
Gajendra Singh <gajendra.singh.min21@itbhu.ac.in>
2022-09-03 14:10:24 +0530

    Added Mul.java

---
+++ Mul.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Mul {
+    public static void main (String args[]){
+        Scanner ab = new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        int a = ab.nextInt();
+        int b = ab.nextInt();
+        int mul = a*b;
+        System.out.println("Product of these number : "+mul);
+    }
+    
+}
