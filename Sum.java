a023e9697e96692bd7bed780f1c973de54a6d2da
Gajendra Singh <gajendra.singh.min21@itbhu.ac.in>
2022-09-03 13:53:50 +0530

    Adding Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+    public static void main (String args[]){
+        Scanner ab = new Scanner(System.in);
+        System.out.println("Enter two numbers -");
+        int a = ab.nextInt();
+        int b = ab.nextInt();
+        int sum = a+b;
+        System.out.println("Sum of these number : "+sum);
+    }
+    
+}
